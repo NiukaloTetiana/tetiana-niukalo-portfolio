c3688f9c8d5f133db34c8e9defd604ff77c86967
Niukalo Tetiana <tanusha.17121994@gmail.com>
2024-12-26 01:39:09 +0200

    add AnimatedCursor

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,49 @@
+import { useEffect } from "react";
+import AnimatedCursor from "react-animated-cursor";
+
 import { ThemeToggle } from "./ThemeToggle";
 
 export const App = () => {
+  useEffect(() => {
+    document.body.classList.add(
+      "bg-gradient-to-r",
+      "from-bgFirstColor",
+      "to-bgSecondColor"
+    );
+    return () => {
+      document.body.classList.remove(
+        "bg-gradient-to-r",
+        "from-bgFirstColor",
+        "to-bgSecondColor"
+      );
+    };
+  }, []);
+
   return (
     <div className="flex h-screen items-center justify-center">
       <ThemeToggle />
       <h1 className="text-4xl text-accentColor">PORTFOLIO</h1>
+      <AnimatedCursor
+        innerSize={7}
+        outerSize={35}
+        color="91, 162, 33"
+        outerAlpha={0.3}
+        innerScale={1}
+        outerScale={1.7}
+        // clickables={[
+        //   "a",
+        //   'input[type="text"]',
+        //   'input[type="email"]',
+        //   'input[type="number"]',
+        //   'input[type="submit"]',
+        //   'input[type="image"]',
+        //   "label[for]",
+        //   "select",
+        //   "textarea",
+        //   "button",
+        //   ".link",
+        // ]}
+      />
     </div>
   );
 };
--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -9,12 +9,12 @@ export const ThemeToggle = () => {
   return (
     <button
       onClick={toggleTheme}
-      className="pointer-events-auto flex items-center justify-center rounded-full bg-textColor p-4 shadow-md transition duration-300 lg:hover:bg-hoverColor lg:hover:shadow-lg"
+      className="pointer-events-auto flex size-12 items-center justify-center rounded-full bg-textColor shadow-md transition duration-300 lg:hover:bg-hoverColor lg:hover:shadow-lg"
     >
       {theme === "light" ? (
         <BsMoonStars className="size-5 fill-bgFirstColor sm-max:size-[14px]" />
       ) : (
-        <TbSunglasses className="size-6 stroke-accentColor sm-max:size-[14px]" />
+        <TbSunglasses className="size-6 stroke-bgFirstColor sm-max:size-[14px]" />
       )}
     </button>
   );
