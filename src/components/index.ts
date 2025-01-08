1cbcf2467a1d8acd1ec079717516fb720cbe4b61
Niukalo Tetiana <tanusha.17121994@gmail.com>
2025-01-08 22:50:49 +0200

    add SideBar component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,5 @@
 export * from "./App";
 export * from "./ThemeToggle";
 export * from "./ScrollUpButton";
-export * from "./Footer";
-export * from "./NavBar";
+// export * from "./Footer";
+export * from "./SideBar";
