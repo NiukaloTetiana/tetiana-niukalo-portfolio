ed2df88352c58746e0ce7d97468af3ae3e1f787a
Niukalo Tetiana <tanusha.17121994@gmail.com>
2025-01-11 00:48:11 +0200

    add textarea support to universal input component

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,71 +1,73 @@
 import { MdOutlineArrowOutward } from "react-icons/md";
-import { useForm } from "react-hook-form";
+import {
+  useForm,
+  SubmitHandler,
+  FieldValues,
+  UseFormRegister,
+} from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
-// import { toast } from "react-toastify";
 
-import { InputField, InputProps } from "./InputField";
+import { InputField } from "./InputField";
 import { contactSchema } from "../schemas";
+
+interface ContactFormValues {
+  name: string;
+  email: string;
+  message: string;
+}
 
 export const ContactForm = () => {
   const {
     register,
-    // reset,
     handleSubmit,
     formState: { errors },
-  } = useForm<InputProps>({
-    mode: "onChange",
+  } = useForm<ContactFormValues>({
+    mode: "onSubmit",
     resolver: yupResolver(contactSchema),
   });
+  const onSubmit: SubmitHandler<ContactFormValues> = (data) => {
+    console.log(data);
+  };
 
   return (
     <form
-      onSubmit={handleSubmit}
+      onSubmit={handleSubmit(onSubmit)}
       className="flex w-full flex-col gap-5 md:gap-9"
     >
       <div className="flex w-full flex-col gap-3 md:flex-row">
         <InputField
+          tag="input"
           label="Your name"
           name="name"
-          type="text"
           placeholder=""
           errors={errors}
-          register={register}
+          register={register as unknown as UseFormRegister<FieldValues>}
         />
-
         <InputField
+          tag="input"
           label="Your email"
           name="email"
-          type="text"
           placeholder=""
           errors={errors}
-          register={register}
+          register={register as unknown as UseFormRegister<FieldValues>}
         />
       </div>
 
-      <div className="relative w-full">
-        <textarea
-          label="Your message"
-          name="message"
-          id="message"
-          rows="7"
-          placeholder=""
-          {...register("message")}
-          className={`peer h-[150px] w-full resize-none rounded-[30px] border bg-transparent px-5 py-3 text-[14px] font-medium leading-[1.44] tracking-[-0.03em] text-textColor placeholder-transparent outline-none hover:border-accentColor focus-visible:border-accentColor md:h-[190px] md:text-[16px] ${
-            errors.message
-              ? "border-red-700 focus-visible:border-red-700 focus-visible:shadow-sm"
-              : "border-secondTextColor focus-visible:shadow-sm"
-          }`}
-        />
-        <label htmlFor="message" className="label">
-          Your message
-        </label>
-        {errors.message && <p className="error">{errors.message.message}</p>}
-      </div>
+      <InputField
+        tag="textarea"
+        label="Your message"
+        name="message"
+        placeholder=""
+        errors={errors}
+        register={register as unknown as UseFormRegister<FieldValues>}
+        rows={7}
+      />
 
       <button className="address-label group custom-transition relative flex h-[42px] w-[200px] items-center justify-between overflow-hidden rounded-full border border-accentColor bg-transparent py-[14px] pl-6 text-start !text-textColor before:absolute before:inset-0 before:origin-right before:scale-x-0 before:bg-accentColor before:transition-transform before:duration-500 hover:before:z-[-1] hover:before:scale-x-100 md:h-[52px]">
         Send message
-        <span className="z-9 custom-transition flex h-[42px] w-[42px] items-center justify-center rounded-full bg-accentColor md:h-[52px] md:w-[52px]">
-          <MdOutlineArrowOutward className="custom-transition z-10 size-5 text-white transition-transform group-hover:-translate-y-10 group-hover:translate-x-4 md:size-[22px]" />
+        <span className="custom-transition relative h-[42px] w-[42px] overflow-hidden rounded-full bg-accentColor shadow-md md:h-[52px] md:w-[52px]">
+          <MdOutlineArrowOutward className="custom-transition absolute left-1/2 top-1/2 z-10 size-5 -translate-x-1/2 -translate-y-1/2 fill-textColor group-hover:-translate-y-10 group-hover:translate-x-8 md:size-[22px]" />
+          <MdOutlineArrowOutward className="custom-transition absolute left-0 top-full size-5 -translate-x-1/2 -translate-y-1/2 fill-textColor group-hover:-translate-y-9 group-hover:translate-x-4 md:size-[22px]" />
         </span>
       </button>
     </form>
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,36 +1,43 @@
-import { InputHTMLAttributes } from "react";
-
+import { InputHTMLAttributes, TextareaHTMLAttributes } from "react";
 import { FieldErrors, FieldValues, UseFormRegister } from "react-hook-form";
 
-export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+type InputProps<T extends "input" | "textarea"> = {
+  tag: T;
   label: string;
   name: string;
   register: UseFormRegister<FieldValues>;
   errors: FieldErrors<FieldValues>;
-}
+} & (T extends "input"
+  ? InputHTMLAttributes<HTMLInputElement>
+  : TextareaHTMLAttributes<HTMLTextAreaElement>);
 
-export const InputField = ({
+export const InputField = <T extends "input" | "textarea">({
+  tag: Tag,
   label,
   name,
   register,
   errors,
   ...props
-}: InputProps) => {
+}: InputProps<T>) => {
   const hasError = !!errors[name];
   const errorMessage = errors[name]?.message as string | undefined;
 
   return (
     <div className="relative w-full">
-      <input
-        className={`peer custom-transition h-[42px] w-full rounded-full border bg-transparent px-5 py-3 text-[14px] font-medium leading-[1.44] tracking-[-0.03em] text-textColor placeholder-transparent md:h-[48px] lg:h-[52px] md:py-[14px] md:text-[16px] ${
+      <Tag
+        id={name}
+        className={`peer custom-transition w-full border bg-transparent px-5 py-3 text-[14px] font-medium leading-[1.44] tracking-[-0.03em] text-textColor placeholder-transparent hover:border-accentColor focus-visible:border-accentColor md:py-[14px] md:text-[16px] ${Tag === "input" ? "h-[42px] rounded-full md:h-[48px] lg:h-[52px]" : "h-[150px] resize-none rounded-[30px] md:h-[190px]"} ${
           hasError
             ? "border-red-700 focus-visible:border-red-700 focus-visible:shadow-sm"
             : "border-secondTextColor focus-visible:border-accentColor focus-visible:shadow-sm"
         }`}
         {...register(name)}
-        {...props}
+        // eslint-disable-next-line
+        {...(props as any)}
       />
-      <label className="label">{label}</label>
+      <label htmlFor={name} className="label">
+        {label}
+      </label>
       {errorMessage && <p className="error">{errorMessage}</p>}
     </div>
   );
