d26c7de5bd9beabc5392373aede36f29cf2ee8b9
Niukalo Tetiana <tanusha.17121994@gmail.com>
2025-01-12 03:54:16 +0200

    add favicon + photo and update About section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,3 +1,6 @@
+import { BsFolderSymlinkFill } from "react-icons/bs";
+import photo from "../assets/images/photo.jpg";
+
 export const About = () => {
   return (
     <section id="about">
@@ -5,6 +8,44 @@ export const About = () => {
         <h2 className="title" data-content="Resume">
           About <span className="text-accentColor">me</span>
         </h2>
+
+        <div className="md:flex md:gap-4 lg:gap-20">
+          <div className="mx-auto mb-5 flex size-[180px] shrink-0 items-center justify-center overflow-hidden rounded-full shadow-md md:mx-0 md:h-[480px] md:w-[320px] md:rounded-[30px] lg:h-[600px] lg:w-[420px]">
+            <img
+              className="h-full w-full object-cover"
+              src={photo}
+              alt="Tetiana Niukalo photo"
+              loading="lazy"
+            />
+          </div>
+
+          <div>
+            <h1 className="custom-transition mb-4 text-center font-fontSecondary text-[38px] uppercase leading-[1.1] text-accentColor md:text-start md:text-[54px] lg:text-[70px]">
+              I&#8217;m Tetiana Niukalo.
+              <span className="block text-[28px] text-textColor md:text-[40px] lg:text-[54px]">
+                Fullstack Developer
+              </span>
+            </h1>
+            <p className="custom-transition mb-6 w-full text-center text-[15px] font-semibold leading-[1.5] tracking-[-0.03em] text-textColor md:w-[360px] md:text-start md:text-[16px] lg:w-[490px] lg:md:text-[18px]">
+              With a passion for continuous learning and growth, I am committed
+              to taking on new challenges with enthusiasm. I take a responsible
+              approach to time management and understand the importance of
+              deadlines. I approach challenges with curiosity and see every
+              experience as an opportunity for self-improvement.
+            </p>
+            <a
+              href="https://drive.google.com/file/d/1rXDNQ1smYqRzwxFxOLD00MdNYlqd0ZPs/view?usp=drive_link"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="button group mx-auto !w-[140px] md:mx-0 md:!w-[180px]"
+            >
+              View CV
+              <span className="button-span flex items-center justify-center bg-hoverColor">
+                <BsFolderSymlinkFill className="custom-transition z-10 size-4 fill-textColor md:size-5" />
+              </span>
+            </a>
+          </div>
+        </div>
       </div>
     </section>
   );
--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -66,9 +66,9 @@ export const ContactForm = () => {
         rows={7}
       />
 
-      <button className="address-label group custom-transition relative flex h-[42px] w-[200px] items-center justify-between overflow-hidden rounded-full border-[1.3px] border-accentColor bg-transparent py-[14px] pl-6 text-start !text-textColor before:absolute before:inset-0 before:origin-right before:scale-x-0 before:bg-hoverColor before:transition-transform before:duration-500 hover:border-hoverColor hover:before:z-[-1] hover:before:scale-x-100 focus-visible:border-hoverColor md:h-[52px]">
+      <button type="submit" className="button group">
         Send message
-        <span className="custom-transition relative h-[42px] w-[42px] overflow-hidden rounded-full bg-accentColor shadow-sm group-hover:bg-hoverColor group-hover:shadow-md group-focus-visible:bg-hoverColor group-focus-visible:shadow-md md:h-[52px] md:w-[52px]">
+        <span className="button-span">
           <MdOutlineArrowOutward className="custom-transition absolute left-1/2 top-1/2 z-10 size-5 -translate-x-1/2 -translate-y-1/2 fill-textColor group-hover:-translate-y-10 group-hover:translate-x-8 md:size-[22px]" />
           <MdOutlineArrowOutward className="custom-transition absolute left-0 top-full size-5 -translate-x-1/2 -translate-y-1/2 fill-textColor group-hover:-translate-y-8 group-hover:translate-x-3 md:size-[22px] md:group-hover:-translate-y-9 md:group-hover:translate-x-4" />
         </span>
