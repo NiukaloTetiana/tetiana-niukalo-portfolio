ed0899ec942e5a7c7489109ea29dcc06cb7695b3
Niukalo Tetiana <tanusha.17121994@gmail.com>
2025-01-14 23:38:54 +0200

    add long dash before heading for desktop

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -19,14 +19,14 @@ export const About = () => {
             />
           </div>
 
-          <div>
-            <h1 className="custom-transition mb-4 text-center font-fontSecondary text-[38px] uppercase leading-[1.1] text-accentColor md:text-start md:text-[54px] lg:text-[70px]">
+          <div className="lg:pt-[180px]">
+            <h1 className="custom-transition relative mb-4 text-center font-fontSecondary text-[38px] uppercase leading-[1.1] text-accentColor before:hidden before:content-['&mdash;'] md:text-start md:text-[54px] lg:text-[70px] lg:before:absolute lg:before:left-[-55px] lg:before:top-0 lg:before:inline-block">
               I&#8217;m Tetiana Niukalo.
               <span className="block text-[28px] text-textColor md:text-[40px] lg:text-[54px]">
                 Fullstack Developer
               </span>
             </h1>
-            <p className="custom-transition mb-6 w-full text-center text-[15px] font-semibold leading-[1.5] tracking-[-0.03em] text-textColor md:w-[360px] md:text-start md:text-[16px] lg:w-[490px] lg:md:text-[18px]">
+            <p className="custom-transition mb-6 w-full text-center text-[15px] font-semibold leading-[1.5] tracking-[-0.03em] text-textColor md:w-[360px] md:text-start md:text-[16px] lg:w-[490px]">
               With a passion for continuous learning and growth, I am committed
               to taking on new challenges with enthusiasm. I take a responsible
               approach to time management and understand the importance of
