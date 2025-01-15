a51bd202cda8ac9639a20901b4f28e427b7095bb
Niukalo Tetiana <tanusha.17121994@gmail.com>
2025-01-15 23:59:08 +0200

    add responsive for My skills part

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -15,7 +15,7 @@ export const About = () => {
           About <span className="text-accentColor">me</span>
         </h2>
 
-        <div className="mb-5 md:flex md:gap-5 lg:gap-20">
+        <div className="mb-10 md:flex md:gap-5 lg:gap-20">
           <div className="mx-auto mb-7 flex size-[180px] shrink-0 items-center justify-center overflow-hidden rounded-full shadow-md md:mx-0 md:mb-0 md:size-[370px] lg:size-[500px]">
             <img
               className="h-full w-full object-cover"
@@ -53,21 +53,22 @@ export const About = () => {
           </div>
         </div>
 
-        <h3 className="custom-transition mb-10 font-fontSecondary text-[28px] uppercase leading-[1.1] text-textColor md:text-[40px] lg:text-[54px]">
+        <h3 className="custom-transition mb-10 text-center font-fontSecondary text-[28px] uppercase leading-[1.1] text-textColor md:text-start md:text-[40px] lg:text-[54px]">
           My skills
         </h3>
 
-        <div className="md:flex md:gap-4">
+        <div className="flex flex-col gap-10 md:flex-row md:flex-wrap md:gap-12">
           <div>
-            <h4 className="custom-transition mb-8 flex items-center gap-2 text-[20px] uppercase text-textColor md:text-[24px]">
+            <h4 className="custom-transition mb-5 flex items-center gap-2 text-[20px] uppercase text-textColor md:mb-8 md:text-[24px]">
               <FaCogs className="size-10 text-accentColor" />
               Development
             </h4>
-            <ul className="flex max-w-[375px] flex-wrap md:max-w-[500px] lg:gap-5">
+
+            <ul className="flex flex-wrap gap-2 md:w-[600px] md:gap-4 lg:max-w-[490px]">
               {developmentSkills.map((item) => (
                 <li
                   key={item}
-                  className="hover custom-transition flex size-[100px] items-center justify-center rounded-full border border-borderColor bg-transparent text-[12px] text-textColor md:text-[14px]"
+                  className="hover custom-transition flex size-[85px] items-center justify-center rounded-full border border-borderColor bg-transparent text-[12px] text-textColor md:size-[100px] md:text-[14px]"
                 >
                   {item}
                 </li>
@@ -76,7 +77,7 @@ export const About = () => {
           </div>
 
           <div>
-            <h4 className="custom-transition mb-8 flex items-center gap-2 text-[20px] uppercase text-textColor md:text-[24px]">
+            <h4 className="custom-transition mb-5 flex items-center gap-2 text-[20px] uppercase text-textColor md:mb-8 md:text-[24px]">
               <BsTools className="size-8 fill-accentColor" />
               Tools
             </h4>
@@ -95,8 +96,8 @@ export const About = () => {
           </div>
 
           <div>
-            <h4 className="custom-transition mb-8 flex items-center gap-2 text-[20px] uppercase text-textColor md:text-[24px]">
-              <FaHandsHelping className="size-8 fill-accentColor" /> Personal
+            <h4 className="custom-transition mb-5 flex items-center gap-2 text-[20px] uppercase text-textColor md:mb-8 md:text-[24px]">
+              <FaHandsHelping className="size-10 fill-accentColor" /> Personal
               Qualities
             </h4>
 
