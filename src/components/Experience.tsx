27c3db22f22206e6658481654bbefe49889938b6
Niukalo Tetiana <tanusha.17121994@gmail.com>
2025-01-18 23:59:54 +0200

    add responsiveness to the Experience component and update styles

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -9,44 +9,53 @@ export const Experience = () => {
           My <span className="text-accentColor">path</span>
         </h2>
 
-        <div className="after:hover after:content:'' custom-transition relative flex flex-col items-center after:absolute after:left-1/2 after:z-[-1] after:h-full after:w-[2px] after:-translate-x-1/2 after:transform after:bg-accentColor">
+        <ul className="custom-transition relative flex flex-col items-end after:absolute after:left-5 after:z-[-1] after:h-full after:w-[2px] after:-translate-x-1/2 after:transform after:bg-accentColor md:items-center md:after:left-1/2">
           {experiences.map((item, index) => {
             const isLeft = index % 2 === 0;
             return (
-              <div
+              <li
                 key={item.id}
-                className={`relative my-8 flex w-[870px] items-center ${
+                className={`group relative my-8 flex items-center md:w-[680px] lg:w-[870px] ${
                   isLeft ? "justify-start" : "justify-end"
                 }`}
               >
-                <div className="hover custom-transition absolute left-1/2 -ml-5 flex size-10 items-center justify-center rounded-full bg-grayColor shadow-sm md:-ml-6 md:size-[48px]">
+                <div className="custom-transition absolute -left-[45px] -ml-5 flex size-[38px] items-center justify-center rounded-full bg-secondTextColor shadow-sm group-hover:bg-hoverColor group-focus-visible:bg-hoverColor sm-max:-left-[28px] md:left-1/2 md:-ml-6 md:size-[48px]">
                   {item.type === "education" ? (
-                    <FaGraduationCap className="fill-textColor" />
+                    <FaGraduationCap className="size-[14px] fill-textColor md:size-[18px]" />
                   ) : (
-                    <FaBriefcase className="fill-textColor" />
+                    <FaBriefcase className="size-[12px] fill-textColor md:size-[16px]" />
                   )}
                 </div>
 
-                {/* Контейнер для контенту */}
                 <div
-                  className={`after:contetn:'' custom-transition relative h-[120px] rounded-[25px] bg-grayColor p-5 after:absolute after:top-[50px] after:border-8 after:border-transparent ${
+                  className={`custom-transition relative flex h-[160px] w-[270px] flex-col rounded-[25px] bg-secondTextColor p-5 after:custom-transition after:absolute after:top-[63px] after:hidden after:border-8 after:border-transparent group-hover:bg-grayColor group-focus-visible:bg-grayColor sm-max:w-[230px] sm-max:p-4 md:h-[145px] md:w-[290px] md:after:block lg:h-[120px] lg:w-[380px] lg:after:top-[50px] ${
                     isLeft
-                      ? "after:-right-4 after:border-l-grayColor"
-                      : "after:-left-4 after:border-r-grayColor"
+                      ? "after:-right-4 after:border-l-secondTextColor after:group-hover:border-l-grayColor after:group-focus-visible:border-l-grayColor"
+                      : "after:-left-4 after:border-r-secondTextColor after:group-hover:border-r-grayColor after:group-focus-visible:border-r-grayColor"
                   }`}
                 >
-                  <h5 className="mb-2 text-xl font-semibold text-accentColor">
+                  <h5 className="address-label mb-3 !text-hoverColor">
                     {item.title}
                   </h5>
-                  <p className="text-textColor">{item.description}</p>
-                  <span className="mt-4 block text-sm text-gray-500">
+                  <p className="description">{item.description}</p>
+                  <p className="address-text mt-3 sm-max:mt-2 sm-max:text-[12px]">
+                    {item.text}
+                  </p>
+
+                  <p
+                    className={`description link mt-auto whitespace-nowrap md:absolute md:top-[63px] md:mt-0 lg:top-[48px] ${
+                      isLeft
+                        ? "md:left-[130%] lg:left-[125%]"
+                        : "md:-left-[57%] lg:-left-[175px]"
+                    }`}
+                  >
                     {item.year}
-                  </span>
+                  </p>
                 </div>
-              </div>
+              </li>
             );
           })}
-        </div>
+        </ul>
       </div>
     </section>
   );
--- src/constants/expiriences.ts
+++ src/constants/expiriences.ts
@@ -2,15 +2,32 @@ export const experiences = [
   {
     id: 1,
     type: "education",
-    title: "Університет А",
-    description: "Ступінь бакалавра в галузі комп'ютерних наук",
-    year: "2015-2019",
+    title: "FullStack Developer",
+    description: "GoIT, Ukraine",
+    year: "2023 - 2024",
   },
   {
     id: 2,
     type: "experience",
-    title: "Компанія X",
-    description: "Позиція: Розробник програмного забезпечення",
-    year: "2015-2019",
+    title: "Economist",
+    description: "A company engaged in the retail trade of petroleum products",
+    year: "2016 - 2020",
+  },
+
+  {
+    id: 3,
+    type: "education",
+    title: "Zhytomyr State Technological University",
+    description: "Master's degree",
+    text: "Public Management and Administration",
+    year: "2017 - 2018",
+  },
+  {
+    id: 4,
+    type: "education",
+    title: "Kyiv National University of Trade and Economics",
+    description: "Bachelor's degree",
+    text: "Accounting and audit",
+    year: "2014-2017",
   },
 ];
